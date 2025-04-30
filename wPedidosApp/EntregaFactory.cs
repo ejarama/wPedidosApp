c18f09223b36e2164b205f08cdc3dde0121b2c46
ejarama <elijllo10@gmail.com>
2025-04-29 21:15:28 -0500

    se implementa patron factory y clase Pedido

---
+++ wPedidosApp/EntregaFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wPedidosApp
+{
+    public static class EntregaFactory
+    {
+        public static IMetodoEntrega CrearEntrega(string tipoProducto, bool urgente, double peso)
+        {
+            
+                if (tipoProducto == "tecnología" && urgente)
+                    return new EntregaDron();
+                
+                else if (tipoProducto == "accesorio")
+                    return new EntregaMoto();
+                else if (tipoProducto == "componente" || peso > 10)
+                    return new EntregaCamion();
+                else
+                    return new EntregaMoto(); // valor por defecto
+
+            
+        }
+    }
+}
