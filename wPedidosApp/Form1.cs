39a27f6ee5b714a60312bddf1593653c7a7a10ad
ejarama <elijllo10@gmail.com>
2025-04-29 21:27:41 -0500

    fix: validación de campos vacíos en el formulario

--- wPedidosApp/Form1.cs
+++ wPedidosApp/Form1.cs
@@ -21,19 +21,38 @@ namespace wPedidosApp
         {
             try
             {
-                string cliente = txtCliente.Text;
-                string producto = cmbProducto.SelectedItem.ToString();
+                string cliente = "";
+                string producto = "";
+                //Validaciones
+                if (string.IsNullOrEmpty(txtCliente.Text.Trim().ToUpper()))
+                    throw new ArgumentException("El nombre del cliente no puede estar vacío");
+                else
+                    cliente = txtCliente.Text.Trim().ToUpper();
+
+                if (cmbProducto.SelectedIndex == -1)
+                    throw new ArgumentException("Debe seleccionar un tipo de producto");
+                else
+                    producto = cmbProducto.SelectedItem.ToString();
+
                 bool urgente = chkUrgente.Checked;
+
                 double peso = Convert.ToDouble(nudPeso.Value);
+                if (peso <= 0)
+                    throw new ArgumentException("El peso debe ser mayor que cero");
+
                 int distancia = Convert.ToInt32(nudDistancia.Value);
+                if (distancia <= 0)
+                    throw new ArgumentException("La distancia debe ser mayor que cero");
+
                 Pedido pedido = new Pedido(cliente, producto, urgente, peso, distancia);
                 RegistroPedidos.Instancia.AgregarPedido(pedido);
                 lblResultado.Text = $"Entrega: {pedido.MetodoEntrega.TipoEntrega()} " +
-                $"Costo: ${pedido.ObtenerCosto():0.00}";
+                                    $"Costo: ${pedido.ObtenerCosto():0.00}";
+                //limpiarCampos();
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error: " + ex.Message);
+                MessageBox.Show("Error: Hay campos vacíos o datos Invalidos. \n" + ex.Message, "Error", MessageBoxButtons.OK);
             }
         }
 
